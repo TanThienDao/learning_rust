bd1bae8b24f2e9f6e3367f766b172b544838401e
Tan Dao <ttdao3@asu.edu>
2026-05-03 14:02:47 -0700

    session 9 struct 157

--- structs/src/test.rs
+++ structs/src/test.rs
@@ -1,8 +1,6 @@
-
-
 #[test]
-fn define_a_sstruct(){
-    let mocha: Coffee = Coffee{
+fn define_a_sstruct() {
+    let mocha: Coffee = Coffee {
         name: String::from("Mocha"),
         price: 4.99,
         is_hot: true,
@@ -10,14 +8,11 @@ fn define_a_sstruct(){
 
     println!(
         "{} costs ${} and is hot: {}",
-        mocha.name,
-        mocha.price,
-        mocha.is_hot
+        mocha.name, mocha.price, mocha.is_hot
     );
 
     let fav_coffee = mocha.name;
     println!("fav_coffee: {}", fav_coffee);
-
 }
 #[derive(Debug)]
 struct Coffee {
@@ -27,7 +22,7 @@ struct Coffee {
 }
 
 #[test]
-fn overwrite_struct_field (){
+fn overwrite_struct_field() {
     let mut beverage: Coffee = Coffee {
         name: String::from("Mocha"),
         price: 4.99,
@@ -39,11 +34,14 @@ fn overwrite_struct_field (){
     println!("beverage : {:?}", beverage);
 }
 #[test]
-fn create_structs_in_a_function(){
+fn create_structs_in_a_function() {
     let name = String::from("Latte");
     let coffee = make_coffee(name, 4.99, true);
     println!("coffee : {:?}", coffee);
-    println!("My {} this morning cost {}. It is {} that it was hot.", coffee.name,coffee.price,coffee.is_hot);
+    println!(
+        "My {} this morning cost {}. It is {} that it was hot.",
+        coffee.name, coffee.price, coffee.is_hot
+    );
 }
 fn make_coffee(name: String, price: f64, is_hot: bool) -> Coffee {
     Coffee {
@@ -60,18 +58,17 @@ fn make_coffee_shorthand(name: String, price: f64, is_hot: bool) -> Coffee {
     }
 }
 #[test]
-fn struct_field_initialization_shorthand_syntax(){
+fn struct_field_initialization_shorthand_syntax() {
     let name = String::from("Latte");
     let price = 4.99;
     let is_hot = true;
     let coffee = make_coffee_shorthand(name, price, is_hot);
     println!("coffee : {:?}", coffee);
-
 }
 #[test]
 fn struct_update_syntax() {
     let mocha: Coffee = make_coffee(String::from("Mocha"), 4.99, true);
-    let caramel_machiato = Coffee{
+    let caramel_machiato = Coffee {
         name: String::from("Caramel Machiato"),
         ..mocha
     };
@@ -83,7 +80,7 @@ fn struct_update_syntax() {
     println!("mocha name: {}", mocha.name);
 }
 #[test]
-fn passing_structs_into_a_function(){
+fn passing_structs_into_a_function() {
     let mut mocha = make_coffee_shorthand(String::from("Mocha"), 4.99, false);
     drink_coffee(&mut mocha);
     println!("mocha : {}", mocha.is_hot);
@@ -95,7 +92,7 @@ fn drink_coffee(coffee: &mut Coffee) {
     coffee.is_hot = false;
 }
 #[test]
-fn deriving_debug_trait_for_struct (){
+fn deriving_debug_trait_for_struct() {
     let value = ["hello", "world"];
     println!("{:?}", value);
     println!("{:#?}", value);
@@ -140,16 +137,24 @@ impl TaylorSwiftSong {
         println!("title: {}", self.title);
         println!("release_year: {}", self.release_year);
         println!("duration_sec: {}", self.duration_sec);
+        println!("Years since release: {}", self.years_since_release());
     }
     fn double_length_reference(&mut self) {
         // Mutable reference to the struct instance( no ownership moved, have permission to mutate)
         self.duration_sec *= 2;
         println!("duration_sec: {:#?}", self);
     }
+    fn is_longer_than(&self, other: &Self) -> bool {
+        // method with multiple parameter in rust.
+        self.duration_sec > other.duration_sec
+    }
+    fn years_since_release(&self) -> u32 {
+        2026 - self.release_year
+    }
 }
 #[test]
-fn defining_struct_methods(){
-    let song = TaylorSwiftSong{
+fn defining_struct_methods() {
+    let song = TaylorSwiftSong {
         title: String::from("Love Story"),
         release_year: 2008,
         duration_sec: 235,
@@ -157,8 +162,8 @@ fn defining_struct_methods(){
     song.display_song_info();
 }
 #[test]
-fn self_parameter_as_mutable_struct_instance(){
-    let song = TaylorSwiftSong{
+fn self_parameter_as_mutable_struct_instance() {
+    let song = TaylorSwiftSong {
         title: String::from("Love Story"),
         release_year: 2008,
         duration_sec: 235,
@@ -170,8 +175,8 @@ fn self_parameter_as_mutable_struct_instance(){
     //println!("length: {}", song.duration_sec);
 }
 #[test]
-fn self_parameter_as_immutable_reference_and_mutable_reference_to_struct_instance(){
-    let mut song = TaylorSwiftSong{
+fn self_parameter_as_immutable_reference_and_mutable_reference_to_struct_instance() {
+    let mut song = TaylorSwiftSong {
         title: String::from("Love Story"),
         release_year: 2008,
         duration_sec: 235,
@@ -182,4 +187,44 @@ fn self_parameter_as_immutable_reference_and_mutable_reference_to_struct_instanc
     // ownership was not taken away from song, this mutable reference will work.
     song.double_length_reference();
     println!("song duration: {}", song.duration_sec);
+}
+#[test]
+fn method_with_multiple_parameters() {
+    let blank_space = TaylorSwiftSong {
+        title: String::from("Blank Space"),
+        release_year: 2014,
+        duration_sec: 231,
+    };
+    let all_too_well = TaylorSwiftSong {
+        title: String::from("All Too Well"),
+        release_year: 2012,
+        duration_sec: 330,
+    };
+    blank_space.is_longer_than(&all_too_well);
+    println!("blank_space: {:?}", blank_space);
+    println!("all too well: {:?}", all_too_well);
+    println!(
+        "is blank space longer than all too well? {}",
+        blank_space.is_longer_than(&all_too_well)
+    );
+
+    if blank_space.is_longer_than(&all_too_well) {
+        println!(            "{} is longer than {}",
+            blank_space.title, all_too_well.title
+        );
+    } else {
+        println!(
+            "{} is longer than {}",
+            all_too_well.title, blank_space.title
+        );
+    }
+}
+#[test]
+fn calling_methods_from_other_methods(){
+    let blank_space = TaylorSwiftSong{
+        title: String::from("Blank Space"),
+        release_year: 2021,
+        duration_sec: 235,
+    };
+    blank_space.display_song_info_reference();
 }
