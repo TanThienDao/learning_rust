786a9f911a73d010329e4b38c1dd16a63d88325f
tandao <tan.th.dao@gmail.com>
2026-05-18 15:42:02 -0700

    save generic section

---
+++ genertic/src/main.rs
@@ -0,0 +1,16 @@
+mod test;
+
+fn main() {
+    println!("Hello, world!");
+}
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+}
+
+pub use crate::front_of_house::hosting::add_to_waitlist;
+
+pub fn eat_at_restaurant() {
+    add_to_waitlist();
+}
