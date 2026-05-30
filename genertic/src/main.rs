402112b56508b6aa649af63555158c6558de6ab6
tandao <tan.th.dao@gmail.com>
2026-05-29 22:46:02 -0700

    generic
    add project section 11
    and lecture test

--- genertic/src/main.rs
+++ genertic/src/main.rs
@@ -1,16 +1,48 @@
 mod test;
-
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+enum DigitalContent {
+    AudioFile,
+    VideoFile
 }
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+
+#[derive(Debug)]
+struct ChatMessage<T> {
+    content: T,
+    time: String,
+}
+impl ChatMessage<DigitalContent> {
+    fn consume_entertaiment(&self){
+        println!("Watching the AudioFile: {:?}", self.content);
     }
 }
-
-pub use crate::front_of_house::hosting::add_to_waitlist;
-
-pub fn eat_at_restaurant() {
-    add_to_waitlist();
+impl<T> ChatMessage<T> {
+    fn retrieve_time(&self) -> String {
+        println!("The message was sent at: {}", self.time);
+        self.time.clone()
+    }
 }
+fn main() {
+    let chat_1 = ChatMessage{
+        content:"string slice",
+        time:"2017-12-13".to_string()
+    };
+    let chat_2 = ChatMessage{
+        content: String::from("String"),
+        time: "2017-12-13".to_string()
+    };
+    let chat_3 = ChatMessage{
+        content: DigitalContent::AudioFile,
+        time: "2017-12-13".to_string()
+    };
+    chat_3.consume_entertaiment();
+
+
+    chat_1.retrieve_time();
+    chat_2.retrieve_time();
+    chat_3.retrieve_time();
+}
+
+
+
+
+
--- genertic/src/test.rs
+++ genertic/src/test.rs
@@ -28,3 +28,115 @@ fn intro_to_generics(){
     println!("{} ", identity(String::from("heap-allocated, growable string")));
     println!("{:?} ", identity(DeliSandwich));
 }
+#[test]
+fn the_tubofish_operator(){
+    println!("{} ", identity::<i8>(5));
+    println!("{} ", identity::<i32>(13));
+    println!("{} ", identity::<f32>(13.25));
+    println!("{} ", identity::<String>(String::from("string slice")));
+    println!("{}", identity::<bool>(true));
+    println!("{:#?}", identity::<DeliSandwich>(DeliSandwich{}));
+}
+fn make_tuple<T>(first: T, secound: i32) -> (T, i32){
+    (first, secound)
+}
+fn make_tuple_2<T,U>(first: T, secound: U) -> (T, U){
+    (first, secound)
+}
+#[test]
+fn multiple_generics(){
+    make_tuple("hello",5);
+    make_tuple_2("hello", 5);
+}
+#[derive(Debug)]
+struct TreasureChest<T> {
+    captain: String,
+    treasure: T,
+}
+impl TreasureChest<String> {
+    fn clean_treasure(&mut self) {
+        self.treasure = self.treasure.trim().to_string();
+    }
+}
+impl TreasureChest<[&str; 3]> {
+    fn list_treasure(&self) {
+        for item in &self.treasure {
+            println!("Treasure item: {}", item);
+        }
+    }
+    fn amount_of_treasure(&self) -> usize{
+        self.treasure.len()
+    }
+
+}
+impl<T> TreasureChest<T> {
+    fn capital_captain(&self) -> String{
+        self.captain.to_uppercase()
+    }
+}
+#[test]
+fn generics_in_structs(){
+    let gold_chest = TreasureChest {
+        captain: String::from("Blackbeard"),
+        treasure: "gold",
+    };
+    let silver_chest = TreasureChest {
+        captain: String::from("Anne Bonny"),
+        treasure: String::from("Silver"),
+    };
+    let special_chest = TreasureChest {
+        captain: String::from("Jack Sparrow"),
+        treasure: ["Gold", "Silver", "Platinum"],
+    };
+    println!("{:#?} ", gold_chest);
+    println!("{:#?} ", special_chest);
+}
+#[test]
+fn generics_and_impl_blocks_I(){
+    let mut silver_chest = TreasureChest {
+        captain: String::from("Anne Bonny"),
+        treasure: String::from("   Silver     "),
+    };
+    silver_chest.clean_treasure();
+    println!("{:#?} ", silver_chest);
+
+    let special_chest = TreasureChest {
+        captain: String::from("Jack Sparrow"),
+        treasure: ["Gold", "Silver", "Platinum"],
+    };
+    special_chest.list_treasure();
+    println!("{:#?} ", special_chest);
+    let size = special_chest.amount_of_treasure();
+    println!("size: {}", size);
+}
+#[test]
+fn generics_and_impl_blocks_II(){
+    let gold_chest = TreasureChest {
+        captain: String::from("Blackbeard"),
+        treasure: "gold",
+    };
+    println!("Captain in uppercase: {}", gold_chest.capital_captain());
+    let special_chest = TreasureChest {
+        captain: String::from("Jack Sparrow"),
+        treasure: ["Gold", "Silver", "Platinum"],
+    };
+    println!("Captain in uppercase: {}", special_chest.capital_captain());
+}
+#[derive(Debug)]
+enum Cheesesteak<T>{
+    Plain,
+    Topping(T),
+}
+#[test]
+fn generics_in_enumd(){
+    let mushroom = Cheesesteak::Topping("mushroonm");
+    let oniion = Cheesesteak::Topping("onionm".to_string());
+    let toping = "bacon".to_string();
+    let bacon = Cheesesteak::Topping(&toping);
+    // let plain = Cheesesteak::Plain; // error: cannot infer type for type parameter `T` declared on the enum `Cheesesteak`
+    let mut plain:Cheesesteak<String> = Cheesesteak::Plain;
+    //plain = Cheesesteak::Topping("mushroom"); // error cannot assign `Cheesesteak<&str>` to `Cheesesteak<String>`
+    plain = Cheesesteak::Topping("mushroom".to_string());
+    println!("{:#?} ", mushroom);
+
+}
